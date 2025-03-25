e21ebdca433d124d3e6b2ef641c78f2ba9fb780d
Grimnirr <philipojedokun942@gmail.com>
2025-03-25 08:42:31 +0100

    style: implement preloader functionality and enhance loader styles for better user experience

---
+++ script.js
@@ -0,0 +1,9 @@
+    // Wait until the page is fully loaded
+    window.addEventListener("load", function () {
+    // Hide the preloader
+    document.getElementById("preloader").style.display = "none";
+
+    // Show the main content
+    document.getElementById("content").style.display = "block";
+});
+        
