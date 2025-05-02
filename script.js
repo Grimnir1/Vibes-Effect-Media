bc8821f2a36af5f536fcfec06fccebf878b4417d
Grimnirr <philipojedokun942@gmail.com>
2025-05-02 21:56:08 +0100

    Add footer and navbar HTML files, and create portfolio CSS styles
    - Introduced footer.html with company information, quick links, and social media connections.
    - Created navbar.html for site navigation with links to Home, Services, Portfolio, About, and Contact.
    - Developed portfolio.css for styling portfolio sections including wedding, portraits, and events with responsive design adjustments.

--- script.js
+++ script.js
@@ -7,3 +7,28 @@
     document.getElementById("content").style.display = "block";
 });
         
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarContainer = document.getElementById('navbar');
+    
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+        navbarContainer.innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+    });
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const navbarContainer = document.getElementById('footer');
+        
+        fetch('footer.html')
+            .then(response => response.text())
+            .then(data => {
+            navbarContainer.innerHTML = data;
+            })
+            .catch(error => console.error('Error loading navbar:', error));
+        });
